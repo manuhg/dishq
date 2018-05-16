a71a9bd480272b7bb1ede78f34e9a5d4df99633c
manu-spherical <manuhegdev@gmail.com>
2018-05-16 21:02:48 +0530

    add embeddings

--- dishq.py
+++ dishq.py
@@ -6,44 +6,65 @@ from sklearn import svm
 from sklearn.manifold import TSNE as tsne
 
 
-DEP_TAGS = ['root', 'nsubj', 'det']
+EMBED_TAGS = ['root', 'nsubj', 'det']
 BOW_TAGS = ['verb', 'adv', 'adj', 'det', 'adv', 'aux', 'noun', 'pron', 'propn', 'nsubj', 'det']
+
 print("Loading spacy en web large..")
 NLP = spacy.load('en_core_web_lg')
 
 def inc_bow(dictionary, key):
     """increment the value at key"""
-    if dictionary.get(key):
+    if key.strip() in dictionary.keys():
         dictionary[key] += 1
 #    else:
 #        dictionary[key]=1
 
 
-def process_sentence(sentence, dim_reduce=False, dims=300):
-    """process each sentence i.e extract only required parts
-       input sentence must be enoced in utf-8 """
-    if  isinstance(sentence, 'unicode'):
-        tokens = NLP(sentence)
-        deps = {}
-        bag_of_words = {key:0 for key in BOW_TAGS}
-        for token in tokens:
-            deptag = token.dep_.lower()
-            postag = token.pos_.lower()
-            inc_bow(bag_of_words, deptag)
-            inc_bow(bag_of_words, postag)
+def add_embeddings(tags_lst, tag_dict, debug_dict, token, tag, dim_reduce=False, dims=300):
+    """given a tag(pos or dependency), add its embeddings value to dictionary"""
+    if (tag in tags_lst) and (tag not in tag_dict.keys()):
+            #include only the first value of a kind
+            embed_vec = None
+            if dims <= 1:
+                dims=1
+                embed_vec = [token.vector_norm] # l2 norm
+            elif dim_reduce and dims < 300:
+                #if we want to reduce the glove vector to smaller dimension. default is 300
+                #dims = 3 if dims>4 else dims #because of a weird bug with tsne
+                embed_vec = list(tsne(n_components=dims).fit_transform([token.vector,token.vector])[0])
+            else:
+                embed_vec = list(token.vector)
+            tag_dict[tag] = embed_vec
+            debug_dict[tag]=token.text
 
-            if (deptag in DEP_TAGS) and (deptag not in deps.keys()):
-                #include only the first value of a kind
+def process_sentence(sentence, dim_reduce=False, dims=300,echo=False):
+    """process each sentence i.e extract only required parts"""
+    tokens = NLP(sentence)
+    embeddings = {}
+    embeds_for_debug={}
+    bag_of_words = {key:0 for key in BOW_TAGS}
+    for token in tokens:
+        deptag = token.dep_.lower()
+        postag = token.pos_.lower()
+        if echo:
+            print(token.text,deptag,postag)
 
-                embed_vec = token.lemma_.vector
-                if dim_reduce and dims < embed_vec.size:
-                    #if we want to reduce the glove vector to smaller dimension.
-                    #default is 300
-                    embed_vec = tsne(n_components=dims).fit_transform(embed_vec)
+        inc_bow(bag_of_words, deptag)
+        inc_bow(bag_of_words, postag)
 
-                    deps[deptag] = embed_vec
-        return sum([sum(deps.values(), []), sum(bag_of_words.values(), [])], [])
-    return list()
+        add_embeddings(EMBED_TAGS,embeddings,embeds_for_debug,token,deptag,dim_reduce,dims)
+        add_embeddings(EMBED_TAGS,embeddings,embeds_for_debug,token,postag,dim_reduce,dims)
+
+
+    for d in EMBED_TAGS:
+        if d not in embeddings.keys():
+            embeddings[d]=[0]*dims
+            embeds_for_debug[d]=''
+
+    if echo:
+        print("\nDeps:\n",embeds_for_debug,"\nbow:\n",bag_of_words)
+    return sum([sum(list(embeddings.values()),[]) ,list(bag_of_words.values())],[])
+
 
 def process_data(x_raw, split_sentence=False, dim_reduce=False, dims=300):
     """extract required data for either training or for prediction """
